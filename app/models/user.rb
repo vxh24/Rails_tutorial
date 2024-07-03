82e1e715ce12ac70d941a52a7b6f6e6092cbae25
vxhoa <vuxuanhoa2403@gmail.com>
2024-07-03 08:05:09 +0700

    secure_password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,16 @@
 class User < ApplicationRecord
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+	before_save :downcase_email
+	validates :name, presence: true,
+                   length: { minimum: 2, maximum: 50 }
+
+  # Validation cho email
+  	validates :email, presence: true,
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: true 
+	has_secure_password
+	private
+	def downcase_email
+		email.downcase!
+	end
 end
