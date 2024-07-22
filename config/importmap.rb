d2c80b1bb969b01d1546eec56f9113f2adc3a030
vxhoa <vuxuanhoa2403@gmail.com>
2024-07-22 16:01:19 +0700

    ok

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,5 +1,3 @@
 # Pin npm packages by running ./bin/importmap
+
 pin "application"
-pin_all_from "app/javascript/controllers", under: "controllers"
-pin_all_from "app/javascript/custom", under: "custom"
-pin "@hotwired/turbo-rails", to: "turbo.min.js"
